e7d17c50b9aab70f195b6e71fb35d9959f80f760
ANGITHA VIJAYAN <ANGITHA VIJAYAN@host.docker.internal>
2023-10-09 11:30:32 +0530

    FirstProgram

---
+++ Maven_Project/src/main/java/NewProject/FirstProgram.java
@@ -0,0 +1,20 @@
+package NewProject;
+
+public class FirstProgram {
+	int x=20;
+	int y=100;
+	int c;
+
+	public void addNumb()
+	{
+		c=x+y;
+		System.out.println(c);
+	}
+		
+		
+
+	
+
+	}
+
+
---
+++ Maven_Project/src/main/java/NewProject/FirstProgram1.java
@@ -0,0 +1,17 @@
+package NewProject;
+
+public class FirstProgram1 extends FirstProgram{
+	
+	public void display()
+	{
+		super.addNumb();
+		System.out.println("hi"+super.x);
+	}
+
+	public static void main(String[] args) {
+FirstProgram1 o=new FirstProgram1();
+o.display();
+//o.addNumb();
+	}
+
+}
